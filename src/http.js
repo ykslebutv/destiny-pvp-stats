8d32779101e34b488ff6295988a0c7399dadaab5
Vadim Tubelsky <vv@samopal.com>
2018-04-13 12:07:15 -0400

    use fecth and remove jquery

--- src/http.js
+++ src/http.js
@@ -1,26 +1,18 @@
-/* global $, Config */
-import Promise from 'es6-promise';
+/* global Config */
 
 class Http {
     request(url) {
-        return new Promise((resolve, reject) => {
-            let headers = {};
-            let fullUrl;
-            if (Config.useProxy) {
-                const encodedUrl = encodeURIComponent(url);
-                fullUrl = `${ Config.proxyUrl }?url=${ encodedUrl }`;
-            } else {
-                headers = { 'x-api-key': Config.apiKey };
-                fullUrl = `${ Config.baseUrl }/${ url }`;
-            }
+        let headers = {};
+        let fullUrl;
+        if (Config.useProxy) {
+            const encodedUrl = encodeURIComponent(url);
+            fullUrl = `${ Config.proxyUrl }?url=${ encodedUrl }`;
+        } else {
+            headers = { 'x-api-key': Config.apiKey };
+            fullUrl = `${ Config.baseUrl }/${ url }`;
+        }
 
-            $.ajax({
-                url: fullUrl,
-                headers: headers,
-                success: res => resolve(res),
-                failure: error => reject(error)
-            });
-        });
+        return fetch(fullUrl, headers).then(res => res.json());
     }
 }
 
