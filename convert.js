b92c064e1b781100d427f5691338bfcdd20552bd
Vadim Tubelsky <vtubelsky@sessionm.com>
2019-11-18 14:44:39 -0500

    convert manifest into sections using schema

--- convert.js
+++ convert.js
@@ -12,23 +12,53 @@ const fs = require('fs');
 
 const apiKey = 'YOUR KEY HERE';
 
-const convert = (manifest, prefix) => {
-    console.log('Converting DestinyActivityDefinition data...');
-    const json = manifest.DestinyActivityDefinition;
+const saveManifest = (section, json) => {
+    const fileName = `./src/manifest/${ section }.json`;
+    console.log(`Writing ${ fileName }`);
 
-    const definitions = Object.assign({},
-        ...Object.keys(json).map(hash => ({ [hash]: json[hash].displayProperties.name })));
+    const jsonStr = JSON.stringify(json, null, 2);
+    console.log(jsonStr);
 
-    const fileName = './src/' + prefix + 'Definitions.json';
-
-    fs.writeFile(fileName, JSON.stringify(definitions), err => {
+    fs.writeFile(fileName, jsonStr, err => {
         if (err) {
             console.log(err);
         }
     });
+}
 
-    console.log('Done!');
-};
+const convertSection = (section, json, schema) => {
+
+    console.log(`Converting ${ section }`);
+
+    const sectionManifest = {};
+
+    // get lowest level name and its value, for example:
+    // for "displayProperties.name" return { "name": "Mayhem" }
+    const getValue = (obj, field) => {
+        const path = field.split('.');
+        let i = 0;
+        let o = obj;
+        while(path[i]) {
+            o = o[path[i]];
+            i = i + 1;
+        }
+        return Object.assign({}, { [path[i-1]]: o });
+    }
+
+    Object.keys(json).map(hash => {
+        const obj = {};
+        schema.map(field => {
+            Object.assign(obj, getValue(json[hash], field));
+        });
+        sectionManifest[hash] = obj;
+    });
+
+    saveManifest(section, sectionManifest);
+}
+
+const convertManifest = (json, schema) => {
+    Object.keys(schema).map(section => convertSection(section, json[section], schema[section]));
+}
 
 const fetchManifestUrl = () => {
     console.log('Getting manifest URL...');
@@ -44,11 +74,29 @@ const fetchManifestUrl = () => {
 
 const fetchManifest = (url) => {
     console.log('Downloading manifest...');
+    const localUrl = 'http://localhost:8003/manifest.json';
     return new Promise((resolve, reject) => {
-        fetch(url).then(res => resolve(res.json()));
+        fetch(localUrl).then(res => resolve(res.json()));
     });
+}
+
+const schema = {
+    "DestinyActivityModeDefinition": [
+        "displayProperties.name",
+        "displayProperties.icon",
+        "modeType",
+        "friendlyName"
+    ],
+    "DestinyActivityDefinition": [
+        "displayProperties.name",
+        "pgcrImage",
+    ],
+    "DestinyStatDefinition": [
+        "displayProperties.name",
+        "displayProperties.icon"
+    ]
 }
 
 fetchManifestUrl()
     .then(url => fetchManifest(url))
-    .then(manifest => convert(manifest, 'map'));
+    .then(manifest => convertManifest(manifest, schema));
